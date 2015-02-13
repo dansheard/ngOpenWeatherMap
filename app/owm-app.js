e54a2a8225a6267ceef249305aeeef5e5e7def45
Dan Sheard <dan.c.sheard@gmail.com>
2015-02-13 11:49:23 +0000

    add city route

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,10 +1,16 @@
 angular.module('OWMApp', ['ngRoute'])
     .config(function($routeProvider){
         $routeProvider.when('/', {
-            templateUrl : './home.html',
+            templateUrl : 'home.html',
             controller : 'HomeCtrl'
+        }).when('/city', {
+        		templateUrl : 'city.html',
+        		controller : 'CityCtrl'
         });
     })
     .controller('HomeCtrl', function($scope) {
         //empty for now
+    });;
+    .controller('CityCtrl', function($scope) {
+    	$scope.city = 'New York';
     });;
