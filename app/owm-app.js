67ebed5f3d696dd05dcfc09c739a25a203e92490
Dan Sheard <dan.c.sheard@gmail.com>
2015-02-14 15:11:05 +0000

    Finish the first steps of routing

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,16 +1,35 @@
 angular.module('OWMApp', ['ngRoute'])
-    .config(function($routeProvider){
+    .value('owmCities', 
+        ['New York', 'Dallas', 'Chicago'])
+  .config(function($routeProvider){
         $routeProvider.when('/', {
-            templateUrl : 'home.html',
-            controller : 'HomeCtrl'
-        }).when('/city', {
-        		templateUrl : 'city.html',
-        		controller : 'CityCtrl'
-        });
+            templateUrl: './home.html',
+            controller : 'HomeCtrl as home'
+        })
+        .when('/cities/:city', {
+            templateUrl : './city.html',
+            controller : 'CityCtrl as city',
+            resolve : {
+                city: function(owmCities, $route, $location) {
+                    var city = $route.current.params.city;
+                    console.log($route.current.params.city)
+                    if(owmCities.indexOf(city) == -1 ) {
+                      $location.path('/error');
+                      return;
+                    }
+                    return city;
+                }
+            }
+        })
+        .when('/error', {
+        template : '<p>Error, Page Not Found</p>'
+    });
     })
-    .controller('HomeCtrl', function($scope) {
-        //empty for now
+    .controller('HomeCtrl', function() {
+      vm = this;
+      vm.welcomeMessage = "Welcome Home";
     })
-    .controller('CityCtrl', function($scope) {
-    	$scope.city = 'New York';
-    });;
+    .controller('CityCtrl', function(city) {
+      vm = this;
+      vm.city = city;
+  });
