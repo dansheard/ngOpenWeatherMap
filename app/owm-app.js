53c45e47cfd66ac63a27424dfcd95628e25de06f
Dan Sheard <dan.c.sheard@gmail.com>
2015-02-17 10:44:24 +0000

    Add animations on changing route events

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,35 +1,49 @@
-angular.module('OWMApp', ['ngRoute'])
-    .value('owmCities', 
-        ['New York', 'Dallas', 'Chicago'])
-  .config(function($routeProvider){
-        $routeProvider.when('/', {
-            templateUrl: './home.html',
-            controller : 'HomeCtrl as home'
-        })
-        .when('/cities/:city', {
-            templateUrl : './city.html',
-            controller : 'CityCtrl as city',
-            resolve : {
-                city: function(owmCities, $route, $location) {
-                    var city = $route.current.params.city;
-                    console.log($route.current.params.city)
-                    if(owmCities.indexOf(city) == -1 ) {
-                      $location.path('/error');
-                      return;
-                    }
-                    return city;
-                }
+angular.module('OWMApp', ['ngRoute', 'ngAnimate'])
+  .value('owmCities', 
+      ['New York', 'Dallas', 'Chicago'])
+  .config(function($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: './home.html',
+      controller : 'HomeCtrl as home'
+    })
+    .when('/cities/:city', {
+      templateUrl : './city.html',
+      controller : 'CityCtrl as city',
+      resolve : {
+          city: function(owmCities, $route, $location) {
+            var city = $route.current.params.city;
+            if(owmCities.indexOf(city) == -1 ) {
+              $location.path('/error');
+              return;
             }
-        })
-        .when('/error', {
-        template : '<p>Error, Page Not Found</p>'
+            return city;
+          }
+      }
+    })
+    .when('/error', {
+      template : '<p>Error, Page Not Found</p>'
     });
-    })
-    .controller('HomeCtrl', function() {
-      vm = this;
-      vm.welcomeMessage = "Welcome Home";
-    })
-    .controller('CityCtrl', function(city) {
-      vm = this;
-      vm.city = city;
+  })
+
+  .run(function($rootScope, $location, $timeout) {
+    $rootScope.$on('$routeChangeError', function() {
+      $location.path("/error");
+    });
+    $rootScope.$on('$routeChangeStart', function() {
+      $rootScope.isLoading = true;
+    });
+    $rootScope.$on('$routeChangeSuccess', function() {
+      $timeout(function() {
+        $rootScope.isLoading = false;
+      }, 1000);
+    });
+  })
+
+  .controller('HomeCtrl', function() {
+    vm = this;
+    vm.welcomeMessage = "Welcome Home";
+  })
+  .controller('CityCtrl', function(city) {
+    vm = this;
+    vm.city = city;
   });
